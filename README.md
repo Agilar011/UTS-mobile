1eaaced45e1b8ace1abbc45cba5654ebe9c612c0
Agilar011 <88954314+Agilar011@users.noreply.github.com>
2023-10-23 14:09:46 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,98 @@
+# week7
+
+
+Laporan Praktikum Pertemuan 7
+Pemrograman Mobile
+1.	Praktikum 1: Membangun Layout di Flutter
+
+
+    1.	Langkah 1
+
+        ![Alt text](image.png)
+        
+        Jadi pada langkah pertama buat project dengan tekan ctrl+shift+p dan pilih opsi diatas. Kemudian pilih lokasi file untuk menyimpan project.
+ 
+
+    2.	Langkah 2 
+
+        ![Alt text](image-1.png)
+
+        Ubah kode sesuai perintah pada langkah 2 susai perintah.
+
+    3.	Langkah 3
+
+        ![Alt text](image-2.png)
+    
+        Identifikasi layout diagram
+  
+    4.	Langkah 4
+
+        ![Alt text](image-3.png)
+        
+        Hasil implementasi kode yang diberikan.
+ 
+
+        1.	Soal 1 
+
+            ![Alt text](image-4.png)
+            
+            Pengubahan property CrossAxisAligment.start
+ 
+
+        2.	Soal 2
+ 
+            ![Alt text](image-5.png)
+            
+            Penambahan padding dan penggantian warna teks.
+        3.	Soal 3
+ 
+            ![Alt text](image-6.png)
+            
+            Pengubahan warna icon dan textnya menjadi merah.
+2.	Praktikum 2: Implementasi button row
+    1.	Langkah 1 : Buat method Column _buildButtonColumn
+ 
+	    ![Alt text](image-7.png)
+        
+        Dalam langkah pertama proses yang dijalankan yaitu pendeklarasian variable dan proses cetak nilai.
+    2.	Langkah 2 : Buat widget buttonSection
+ 
+        ![Alt text](image-8.png)
+        
+        Penambahan widget buttonSection
+    3.	Langkah 3 : Tambah button section ke body
+
+        ![Alt text](image-9.png)
+
+
+	 
+3.	Praktikum 3: Implementasi text section
+    1.	Langkah 1 : Buat widget textSection
+  
+	    ![Alt text](image-10.png)
+        
+        Penambahan text section.
+    2.	Langkah 2 : Tambahkan variabel text section ke body
+ 
+        ![Alt text](image-11.png)
+        
+        Penambahan text section.
+4.	Praktikum 4: Implementasi image section
+    1.	Langkah 1 : Siapkan aset gambar
+ 
+        ![Alt text](image-12.png)
+        
+        Saya membuat folder baru dengan foto yang dimaksud.
+    2.	Langkah 2 : Tambahkan gambar ke body
+ 
+        ![Alt text](image-13.png)
+        
+        Saya menambahkan foto ke body.
+    3.	Langkah 3 : : Terakhir, ubah menjadi ListView
+ 
+        ![Alt text](image-14.png)
+        
+        Pengubahan menjadi listview.
+
+4.	Kumpulkan berupa link commit repo GitHub 
+-	https://github.com/Agilar011/Mobile_week7
---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  // Metode pembantu untuk membangun kolom dengan widget sesuai warna, ikon, dan teks
+  Widget buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          color: color,
+        ),
+        const SizedBox(height: 8.0), // Spasi antara ikon dan teks
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 16.0, // Atur ukuran teks sesuai kebutuhan Anda
+            fontWeight: FontWeight.w400,
+            color: color,
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
+    Widget titleSection = Container(
+      padding: EdgeInsets.all(32.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: const Text(
+                    'Wisata Gunung di Batu',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Batu, Malang, Indonesia',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          buildButtonColumn(
+              Colors.red, Icons.star, '41'), // Memanggil metode pembantu
+        ],
+      ),
+    );
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        buildButtonColumn(color, Icons.call, 'CALL'),
+        buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+        'Carilah teks di internet yang sesuai '
+        'dengan foto atau tempat wisata yang ingin '
+        'Anda tampilkan. '
+        'Tambahkan nama dan NIM Anda sebagai '
+        'identitas hasil pekerjaan Anda. '
+        'Selamat mengerjakan 🙂.',
+        softWrap: true,
+      ),
+    );
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My App'),
+        ),
+        body: ListView(
+          children: [
+            Image.asset(
+              'images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection, // Menambahkan bagian teks ke dalam tampilan
+          ],
+        ),
+      ),
+    );
+  }
+}
